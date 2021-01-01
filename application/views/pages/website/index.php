e48f1b9dec25488e510d08efef908a66a2fcd33c
JoyDas <joydascsepuc@gmail.com>
2021-01-01 20:46:34 +0600

    v21---With Coder's Info

--- application/views/pages/website/index.php
+++ application/views/pages/website/index.php
@@ -1,2 +1,10 @@
-Solved By::::
-<p class="font-weight-bold">Joy Das</p>
+<div class="container text-center">
+	<h1 class="text-success">Solved By</h1>
+	<h5>Name: Joy Das</h5>
+	<p>E-mail: joydascsepuc@gmail.com</p>
+	<p>Contact: +8801831586368</p>
+	<p>My GitHub Link: <a href="https://github.com/joydascsepuc" target="_blank">https://github.com/joydascsepuc</a></p>
+	<p>Project Link: <a href="https://github.com/joydascsepuc/codefixit.git" target="_blank">https://github.com/joydascsepuc/codefixit.git</a></p>
+	<br><br><br>
+	<p class="text-success font-weight-bold" style="font-size: 1.3rem;">Good Day & <br>Thank You...!!!</p>
+</div>
